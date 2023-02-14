346bc08e498d93e1be783d549f23d7f3082cfc49
Jeshreen Balraj <jeshreen.balraj@itelasoft.com>
2023-02-14 09:48:32 +0530

    updating the snap shot

--- tests/unit/userCard.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount, shallow } from '@vue/test-utils'
-import UserCard from '@/components/UserCard'
-
-/**
- * Test 1 - Mounting the User card component
- */
-describe('Mount Component', () => {
-    const wrapper = shallow(UserCard);
-})
